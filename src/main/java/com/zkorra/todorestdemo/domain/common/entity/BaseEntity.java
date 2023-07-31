642cfe289a2d5401eabca33a4047f826cb12f115
Korrawich Khosripetch <korrawich.dev@outlook.com>
2023-07-31 16:54:33 +0700

    add createdAt and UpdatedAt columns

--- src/main/java/com/zkorra/todorestdemo/domain/common/entity/BaseEntity.java
+++ src/main/java/com/zkorra/todorestdemo/domain/common/entity/BaseEntity.java
@@ -3,12 +3,17 @@ package com.zkorra.todorestdemo.domain.common.entity;
 import lombok.Getter;
 import lombok.Setter;
 import org.hibernate.annotations.GenericGenerator;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import javax.persistence.*;
+import java.time.LocalDateTime;
 
 @Getter
 @Setter
 @MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
 public class BaseEntity {
 
     @Id
@@ -16,5 +21,13 @@ public class BaseEntity {
     @GeneratedValue(strategy = GenerationType.IDENTITY, generator = "uuid2")
     @Column(nullable = false)
     protected String id;
+
+    @CreatedDate
+    @Column(name = "created_at")
+    protected LocalDateTime createdAt;
+
+    @LastModifiedDate
+    @Column(name = "updated_at")
+    protected LocalDateTime updatedAt;
 
 }
