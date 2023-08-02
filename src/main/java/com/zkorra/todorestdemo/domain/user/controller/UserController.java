7f2dac26ba71410989eb17abba8b14ec3913fb6a
Korrawich Khosripetch <korrawich.dev@outlook.com>
2023-08-02 12:42:57 +0700

    get current user controller

--- src/main/java/com/zkorra/todorestdemo/domain/user/controller/UserController.java
+++ src/main/java/com/zkorra/todorestdemo/domain/user/controller/UserController.java
@@ -2,9 +2,12 @@ package com.zkorra.todorestdemo.domain.user.controller;
 
 import com.zkorra.todorestdemo.domain.user.dto.UserDto;
 import com.zkorra.todorestdemo.domain.user.service.UserService;
+import com.zkorra.todorestdemo.security.AuthUserDetails;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -28,5 +31,10 @@ public class UserController {
     @PostMapping("/login")
     public ResponseEntity<UserDto> login(@RequestBody UserDto.Login login) {
         return ResponseEntity.ok(userService.login(login));
+    }
+
+    @GetMapping()
+    public ResponseEntity<UserDto> currentUser(@AuthenticationPrincipal AuthUserDetails authUserDetails) {
+        return ResponseEntity.ok(userService.currentUser(authUserDetails));
     }
 }
