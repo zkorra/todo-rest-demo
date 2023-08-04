7760ef42c5489ca7a8ae2da434da6a0733e1e07d
Korrawich Khosripetch <korrawich.dev@outlook.com>
2023-08-04 16:22:00 +0700

    add spring validations

--- src/main/java/com/zkorra/todorestdemo/exceptions/ControllerExceptionHandler.java
+++ src/main/java/com/zkorra/todorestdemo/exceptions/ControllerExceptionHandler.java
@@ -2,36 +2,48 @@ package com.zkorra.todorestdemo.exceptions;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
-import java.util.Date;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @ControllerAdvice
 public class ControllerExceptionHandler {
 
     @ExceptionHandler(InvalidInputException.class)
     public ResponseEntity<ErrorResponse> handleInvalidInputException(InvalidInputException e) {
-        return responseErrorMessage(HttpStatus.BAD_REQUEST, e.getMessage());
+        return responseErrorMessage(HttpStatus.BAD_REQUEST, List.of(e.getMessage()));
     }
 
     @ExceptionHandler(ResourceNotFoundException.class)
     public ResponseEntity<ErrorResponse> handleResourceNotFoundException(ResourceNotFoundException e) {
-        return responseErrorMessage(HttpStatus.NOT_FOUND, e.getMessage());
+        return responseErrorMessage(HttpStatus.NOT_FOUND, List.of(e.getMessage()));
     }
 
     @ExceptionHandler(ResourceConflictException.class)
     public ResponseEntity<ErrorResponse> handleResourceConflictException(ResourceConflictException e) {
-        return responseErrorMessage(HttpStatus.CONFLICT, e.getMessage());
+        return responseErrorMessage(HttpStatus.CONFLICT, List.of(e.getMessage()));
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ErrorResponse> handleMethodArgumentNotValidException(MethodArgumentNotValidException e) {
+        List<String> messages = e.getBindingResult()
+                .getFieldErrors()
+                .stream()
+                .map(error -> String.format("%s %s", error.getField(), error.getDefaultMessage()))
+                .collect(Collectors.toList());
+        return responseErrorMessage(HttpStatus.BAD_REQUEST, messages);
     }
 
     @ExceptionHandler(Exception.class)
     public ResponseEntity<ErrorResponse> handleException(Exception e) {
-        return responseErrorMessage(HttpStatus.INTERNAL_SERVER_ERROR, "internal server error");
+        return responseErrorMessage(HttpStatus.INTERNAL_SERVER_ERROR, List.of("internal server error"));
     }
 
-    private ResponseEntity<ErrorResponse> responseErrorMessage(HttpStatus status, String message) {
-        ErrorResponse error = new ErrorResponse(status.value(), new Date(), message);
+    private ResponseEntity<ErrorResponse> responseErrorMessage(HttpStatus status, List<String> errors) {
+        ErrorResponse error = new ErrorResponse(status.value(), status.getReasonPhrase(), errors);
         return new ResponseEntity<>(error, status);
     }
 }
--- src/main/java/com/zkorra/todorestdemo/exceptions/ErrorResponse.java
+++ src/main/java/com/zkorra/todorestdemo/exceptions/ErrorResponse.java
@@ -1,19 +1,24 @@
 package com.zkorra.todorestdemo.exceptions;
 
+import com.fasterxml.jackson.annotation.JsonTypeInfo;
+import com.fasterxml.jackson.annotation.JsonTypeName;
 import lombok.Getter;
 
-import java.util.Date;
+import java.util.List;
 
 @Getter
+@JsonTypeName("error")
+@JsonTypeInfo(use = JsonTypeInfo.Id.NAME, include = JsonTypeInfo.As.WRAPPER_OBJECT)
 public class ErrorResponse {
 
     private final int code;
-    private final Date timestamp;
     private final String message;
+    private final List<String> errors;
 
-    public ErrorResponse(int code, Date timestamp, String message) {
+    public ErrorResponse(int code, String message, List<String> errors) {
         this.code = code;
-        this.timestamp = timestamp;
         this.message = message;
+        this.errors = errors;
     }
+
 }
