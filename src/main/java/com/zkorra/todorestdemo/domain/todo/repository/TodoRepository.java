329025ba07f4aaa478bf6f823fe5026ab5f33786
Korrawich Khosripetch <korrawich.dev@outlook.com>
2023-08-07 14:56:34 +0700

    add delete todos query

--- src/main/java/com/zkorra/todorestdemo/domain/todo/repository/TodoRepository.java
+++ src/main/java/com/zkorra/todorestdemo/domain/todo/repository/TodoRepository.java
@@ -2,6 +2,9 @@ package com.zkorra.todorestdemo.domain.todo.repository;
 
 import com.zkorra.todorestdemo.domain.todo.entity.TodoEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
@@ -10,4 +13,8 @@ import java.util.Optional;
 public interface TodoRepository extends JpaRepository<TodoEntity, String> {
 
     Optional<TodoEntity> findBySlug(String slug);
+
+    @Modifying
+    @Query("delete from TodoEntity t where t.user.id=:userId")
+    int deleteTodosByUserId(@Param("userId") String userId);
 }
